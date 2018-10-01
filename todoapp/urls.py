7508d737f344f29bb02f6f8626e4bd3bcb813592
chrislupdx <christopherlu.pdx@gmail.com>
2018-09-30 17:46:29 -0700

    got user accounts in, folks can only edit/access their own content

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -5,6 +5,7 @@ app_name = 'todoapp' # for namespacing
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_todo', views.add_todo, name='add_todo'),
+    path('loginnav', views.index, name='loginnav'),
     path('delete_todo/<int:pk>/', views.delete_todo, name='delete_todo'),
     path('finished_task/<int:pk>/', views.finished_task, name='finished_task'),
     path('todo_display/', views.todo_display, name='todo_display'),
--- todoapp/views.py
+++ todoapp/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.http import HttpResponse, HttpRequest
 from .models import Todo
 from . import views
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django import forms
 from .forms import Todoform
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest, JsonResponse
 
 def index(request):
-	todo_list = Todo.objects.all()
+	if request.user.is_authenticated:
+		todo_list = Todo.objects.filter(user=request.user)
+	else :
+		todo_list = []
 	context = {'todo':todo_list}
 	return render(request,'todoapp/index.html', context)
 
+@login_required
 def add_todo(request):
 	if request.method == 'POST':
 		todotext = request.POST.get('todo_text')
@@ -24,11 +30,13 @@ def todo_display(request):
 	context = {'todo' :todo}
 	return render(request,'lfw/todo_display.html', context)
 
+@login_required
 def delete_todo(request, pk):
 	todo = get_object_or_404(Todo, pk=pk)
 	todo.delete()
 	return redirect('todoapp:index')
-
+	
+@login_required
 def finished_task(request, pk):
 	todo = get_object_or_404(Todo, pk=pk)
 	todo.finished = not todo.finished
